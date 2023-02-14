c8ad6fe06f09310d9273f6e4bb18dd549a4af56e
Shivam Nishad <shivamnishad4017@gmail.com>
2023-02-14 22:30:40 +0530

    s4

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -6,9 +6,34 @@ public class Pizza {
     private Boolean isVeg;
     private String bill;
 
+    private int toppings;
+
+
+    boolean isExtraCheeseAdded;
+    boolean isExtraToppingAdded;
+    boolean isBillCreated;
+    boolean isTakeAway;
+
+
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg){
+            this.price=300;
+            this.toppings= 70;
+
+        }
+        else{
+            this.price =400;
+            this.toppings=120;
+        }
+
+        this.isExtraToppingAdded= false;
+        this.isExtraCheeseAdded= false;
+        this.isBillCreated= false;
+        this.isTakeAway= false;
+
+        this.bill = "Base Price Of The Pizza: " +this.price+"\n";
     }
 
     public int getPrice(){
@@ -17,18 +42,50 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if(!isExtraCheeseAdded){
+            this.price +=80;
+            this.isExtraCheeseAdded=true;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(!isExtraToppingAdded){
+            this.price += this.toppings;
+            isExtraToppingAdded = true;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+
+        if(!isTakeAway){
+            this.price += 20;
+            isTakeAway = true;
+        }
     }
 
     public String getBill(){
         // your code goes here
-        return this.bill;
+
+        if(!isBillCreated) {
+            if (isExtraCheeseAdded) {
+                this.bill += "Extra Cheese Added: 80"+"\n";
+            }
+
+            if (isExtraToppingAdded) {
+                this.bill += "Extra Toppings Added: 120"+"\n";
+            }
+
+            if (isTakeAway) {
+                this.bill += "Paperbag Added: 20"+"\n";
+            }
+
+            this.bill += "Total Price: "+this.price;
+            this.isBillCreated = true;
+            return this.bill;
+
+        }
+        return "";
     }
 }
